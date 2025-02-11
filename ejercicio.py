38acc1c7a1aad7291648b614bee422325b53ddfb
Ka194 <luzkarinareyes5@gmail.com>
2025-02-11 17:42:36 -0600

    otro commit realizado

---
+++ ejercicio.py
@@ -0,0 +1,6 @@
+nombre = input("ingresa tu nombre")
+carrera = input("Ingresa tu carrera")
+semestre = int(input("Ingresa tu semestre"))
+
+
+print("nombre", nombre, "carrera ", carrera, "semestre ", semestre)
