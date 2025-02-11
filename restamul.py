82688b96ed4a8554bc42ed55813419f1fb9a3485
Ka194 <luzkarinareyes5@gmail.com>
2025-02-11 17:58:15 -0600

    siguiente commit

---
+++ restamul.py
@@ -0,0 +1,14 @@
+
+
+numero1 = int(input("Ingresa el primer numero"))
+numero2 = int(input("Ingresa el segundo numero"))
+
+
+restudado1 = numero1 - numero2
+
+print("el resultado de la resta es : ", restudado1)
+
+resultado2 = numero1 * numero2
+print("el resultado de la resta es : ", resultado2)
+
+
